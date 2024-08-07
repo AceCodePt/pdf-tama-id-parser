656c9023302e1b69644416e3ef9d48639d4f125d
Sagi Carmel <ace.code.pt@gmail.com>
2024-08-07 12:48:23 +0300

    chore: doing axios

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "http2";
+import axios from "axios";
 import fs from "fs/promises";
 import PdfParse from "pdf-parse";
 
@@ -43,25 +43,23 @@ function reduceToTextId(text: string) {
 async function getData(userId: string) {
   let responseText: string = "";
   try {
-    responseText = await fetch("http://127.0.0.1:18000/", {
-      credentials: "omit",
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
-        Accept: "*/*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Content-Type": "text/plain;charset=UTF-8",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "same-origin",
-        Priority: "u=0",
-      },
-      referrer: "http://127.0.0.1:18000/",
-      body: `year=New2&startfrom=0&p1=${userId}&p2=&p3=&p4=&p5=&p6=&p7=&p40=0`,
-      method: "POST",
-      mode: "cors",
-    }).then((res) => res.text());
+    responseText = await axios
+      .post("http://127.0.0.1:18000/", `year=New2&startfrom=0&p1=${userId}&p2=&p3=&p4=&p5=&p6=&p7=&p40=0`, {
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
+          Accept: "*/*",
+          "Accept-Language": "en-US,en;q=0.5",
+          "Content-Type": "text/plain;charset=UTF-8",
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-origin",
+          Priority: "u=0",
+        },
+      })
+      .then((res) => res.data);
   } catch (error) {
     responseText = error.cause;
+    console.error("error", error);
   }
   console.log(responseText);
   return [...(/((05\d)|(0\d))-\d{7}/g.exec(responseText) || [])];
